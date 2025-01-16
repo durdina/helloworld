9fb68f5b37adea10e12dddfb6a6bc4e4cefa5198
Michal Durdina <durdina@pobox.sk>
2025-01-16 12:53:02 +0100

    Adding controller and corresponding integration test

---
+++ src/test/java/com/example/helloworld/it/CheckHTTPResponse.java
@@ -0,0 +1,31 @@
+package com.example.helloworld.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void helloStringMatches() throws Exception {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello World");
+    }
+
+    @Test
+    public void goodbyeStringMatches() throws Exception {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/goodbye",
+                String.class)).contains("Goodbye World");
+    }
+
+}
